f66d9a309eefbf81b90d24a23ce30f3d814648bf
AnishCR <anishcr@gmail.com>
2020-07-27 23:52:32 +0530

    MLD6theJune Python Assignments 3

---
+++ MLD6thJune/Assignments/Python-Assignment-3/list_comprehensions.py
@@ -0,0 +1,21 @@
+# Implement List comprehensions to produce the following lists.
+# Write List comprehensions to produce the following Lists
+# ['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+# ['x', 'y', 'z', 'xx', 'yy', 'zz', 'xxx', 'yyy', 'zzz', 'xxxx', 'yyyy', 'zzzz']
+# [[2], [3], [4], [3], [4], [5], [4], [5], [6]]
+# [[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+# [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
+
+
+l = ['x', 'y', 'z']
+# ['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+print([i * j for i in l for j in range(1,5)])
+
+# ['x', 'y', 'z', 'xx', 'yy', 'zz', 'xxx', 'yyy', 'zzz', 'xxxx', 'yyyy', 'zzzz']
+print([i * j for j in range(1,5) for i in l])
+
+# [[2], [3], [4], [3], [4], [5], [4], [5], [6]]
+print([[j] for i in range(2, 5) for j in range(i, i+3)]) 
+
+# [[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+print([[i + j for i in range(0, 4)] for j in range(2, 6)]) 
---
+++ MLD6thJune/Assignments/Python-Assignment-3/reduce_filter.py
@@ -0,0 +1,37 @@
+# 1.1 Write a Python Program to implement your own myreduce() function which works exactly
+#     like Python's built-in function reduce()
+#
+# 1.2 Write a Python program to implement your own myfilter() function which works exactly
+#     like Python's built-in function filter()
+
+def myreduce(function, iterable, initializer=None) :
+  it = iter(iterable)
+
+  if initializer is None :
+    try:
+      initializer = next(it)
+    except StopIteration:
+      raise TypeError('myreduced called with empty sequence')
+
+  accu = initializer
+
+  for x in it:
+    accu = function(x, accu)
+
+  return accu
+
+def myfilter(function, iterable) :
+  return myreduce(lambda x, accu : accu + [x] if function(x) else accu, iterable, initializer=[])
+
+if __name__ == "__main__":
+  my_list = [1, 2, 3, 4, 5]
+
+  result = myreduce(lambda x, accu : x + accu, my_list)
+
+  print("Sum of list using myreduce is : {}\n".format(result))
+
+  my_list = [12, 65, 54, 39, 102, 339, 221, 50, 70, ]
+
+  result = list(myfilter(lambda x: (x % 13 == 0), my_list))
+
+  print("Filtered list of multiples of 13 using myfilter is : {}\n".format(result))
